33b982904c1f51bb282b401e398c7254c129708a
Imanuel Nusantara Jodi <48381675+ItsJoel88@users.noreply.github.com>
2019-07-31 20:20:02 +0700

    Update Exercises7.js

--- Exercises7.js
+++ Exercises7.js
@@ -1,27 +1,27 @@
 /*NOMOR 1*/
-// var rows1 = 5;
-// for(var i=1;i<=rows1;i++){
-//     console.log('*');
-// }
+ var rows1 = 5;
+ for(var i=1;i<=rows1;i++){
+     console.log('*');
+ }
 
 /*NOMOR 2*/
-// var rows2 = 5;
-// for(var i=1;i<=rows2;i++){
-//     var a ='*';
-//     for(var j=1;j<=i;j++){
-//         var b = '*';
-//         for(var k=1;k<=j;k++){
-//             var c = '*';
-//             for(var l=1;l<=k;l++){
-//                 var d = '*';
-//                 for(var m=1;m<=l;m++){
-//                     var e = '*';
-//                 }
-//             }
-//         }
-//     }
-//     console.log(a,b,c,d,e);
-// }
+ var rows2 = 5;
+ for(var i=1;i<=rows2;i++){
+     var a ='*';
+     for(var j=1;j<=i;j++){
+         var b = '*';
+         for(var k=1;k<=j;k++){
+             var c = '*';
+             for(var l=1;l<=k;l++){
+                 var d = '*';
+                 for(var m=1;m<=l;m++){
+                     var e = '*';
+                 }
+             }
+         }
+     }
+     console.log(a,b,c,d,e);
+ }
 
 /*NOMOR 3*/
 var rows3 = 5;
